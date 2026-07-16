247b6c3696672a5cc89f08f9c500615651017129
AYUSH-1304 <tusharayush2003@gmail.com>
2026-07-16 13:27:40 +0530

    recursive binary search

---
+++ recurcive_binary_search.py
@@ -0,0 +1,21 @@
+def recurcive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+          midpoint = (len(list)) // 2
+          if list[midpoint] == target:
+              return True
+          else:
+              if list[midpoint] < target:
+               return recurcive_binary_search(list[midpoint + 1:], target)
+              else:
+               return recurcive_binary_search(list[:midpoint], target)
+              
+def verify(result):
+   print("Target found: ", result)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = recurcive_binary_search(numbers, 5)
+verify(result)
+
+result = recurcive_binary_search(numbers, 12)
+verify(result)
