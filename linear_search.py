26e6d8875178395df452003cf904705be7f215f2
AYUSH-1304 <tusharayush2003@gmail.com>
2026-07-16 12:17:09 +0530

    linear search complete

--- linear_search.py
+++ linear_search.py
@@ -17,5 +17,5 @@ def verify(index):
         print("Target not found in the list")
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-result = linear_search(numbers, 12)
+result = linear_search(numbers, 5)
 verify(result)
